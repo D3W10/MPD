95cc1b2dc60e2c5a97965f986b87fe7c7a936253
tommyfigs <tomasfvfx@outlook.com>
2024-02-29 15:59:29 +0000

    Criação da class SmartPhone

--- trab1/src/main/java/pt/leirt/mpd/products/SmartPhone.java
+++ trab1/src/main/java/pt/leirt/mpd/products/SmartPhone.java
@@ -1,5 +1,32 @@
 package pt.leirt.mpd.products;
 
-public class SmartPhone {
-    // TO Complete
+public class SmartPhone extends BaseElectronics{
+
+    private final Resolution res;
+    private final double screenSize;
+    private final int batteryCapacity;
+
+    public SmartPhone(String name, String brand, double price, Resolution res, double screenSize, int batteryCapacity){
+        super(name, brand, price);
+        this.res = res;
+        this.screenSize = screenSize;
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    @Override
+    public Category getCategory() {
+        return Category.COMMUNICATIONS;
+    }
+
+    public Resolution getRes() {
+        return res;
+    }
+
+    public double getScreenSize() {
+        return screenSize;
+    }
+
+    public int getBatteryCapacity() {
+        return batteryCapacity;
+    }
 }
