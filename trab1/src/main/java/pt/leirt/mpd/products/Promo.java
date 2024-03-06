a54080c8656247295aba434b207096499b2e3e78
tommyfigs <tomasfvfx@outlook.com>
2024-03-06 16:03:49 +0000

    Criação da class Promo, implementação da sua funcionalidade, aplicar uma promoção a um produto existente

--- trab1/src/main/java/pt/leirt/mpd/products/Promo.java
+++ trab1/src/main/java/pt/leirt/mpd/products/Promo.java
@@ -1,5 +1,36 @@
 package pt.leirt.mpd.products;
 
-public class Promo {
-    // TO Complete
+public class Promo implements Electronics{
+
+    private final double discount;
+    private final Electronics product;
+
+    public Promo(double discount, Electronics product){
+        this.discount = discount;
+        this.product = product;
+    }
+
+    @Override
+    public String getName() {
+        return product.getName();
+    }
+
+    @Override
+    public double getPrice() {
+        return product.getPrice() * discount;
+    }
+
+    @Override
+    public Category getCategory() {
+        return product.getCategory();
+    }
+
+    @Override
+    public String getBrand() {
+        return product.getBrand();
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
 }
