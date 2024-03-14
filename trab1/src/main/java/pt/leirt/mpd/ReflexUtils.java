12aa504ee08f3595195dc91bcb0f7a1a37f455b6
eduardo <duda.louco8@hotmail.com>
2024-03-14 15:22:51 +0000

    Implementação da Class ReflexUtils

---
+++ trab1/src/main/java/pt/leirt/mpd/ReflexUtils.java
@@ -0,0 +1,13 @@
+package pt.leirt.mpd;
+
+import java.io.Writer;
+
+public class ReflexUtils {
+    public void saveToFile(Object o, String fileName){
+
+    }
+
+    public void saveToWriter(Object o, Writer w){
+
+    }
+}
