6c03124d6da041ace07b923ac8f73a0e52aac516
tommyfigs <tomasfvfx@outlook.com>
2024-03-14 15:43:26 +0000

    feat: implementação do método saveToFile

--- trab1/src/main/java/pt/leirt/mpd/ReflexUtils.java
+++ trab1/src/main/java/pt/leirt/mpd/ReflexUtils.java
@@ -1,10 +1,25 @@
 package pt.leirt.mpd;
 
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Field;
 
 public class ReflexUtils {
-    public void saveToFile(Object o, String fileName){
+    public void saveToFile(Object o, String fileName) throws IOException{
+        JSONObject objectJson = new JSONObject();
+        Field[] objectFields = o.getClass().getDeclaredFields();
 
+        for (var f : objectFields){
+            objectJson.put(f.getName(),f);
+        }
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+        bw.write(objectJson.toString());
+        bw.close();
     }
 
     public void saveToWriter(Object o, Writer w){
