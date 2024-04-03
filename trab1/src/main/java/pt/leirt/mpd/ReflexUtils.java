268761df3d1daf3de878bfffada64c226694f4e3
tommyfigs <tomasfvfx@outlook.com>
2024-04-03 15:45:50 +0100

    feat: implementação da class ReflexUtils

--- trab1/src/main/java/pt/leirt/mpd/ReflexUtils.java
+++ trab1/src/main/java/pt/leirt/mpd/ReflexUtils.java
@@ -2,27 +2,46 @@ package pt.leirt.mpd;
 
 import org.json.JSONObject;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ReflexUtils {
-    public void saveToFile(Object o, String fileName) throws IOException{
+    public static void saveToFile(Object o, String fileName) throws IOException, IllegalAccessException {
         JSONObject objectJson = new JSONObject();
-        Field[] objectFields = o.getClass().getDeclaredFields();
+        List<Field> objectFields = getAllFields(o.getClass());
 
         for (var f : objectFields){
-            objectJson.put(f.getName(),f);
+            f.setAccessible(true);
+            objectJson.put(f.getName(), f.get(o));
         }
 
-        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
-        bw.write(objectJson.toString());
-        bw.close();
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))){
+            bw.write(objectJson.toString());
+        }
     }
 
-    public void saveToWriter(Object o, Writer w){
+    public static void saveToWriter(Object o, Writer w) throws IOException {
+        try(PrintWriter fw = new PrintWriter(new FileWriter("output.txt"))){
+            List<Field> objectFields = getAllFields(o.getClass());
 
+            for (var f : objectFields){
+                fw.printf("%s: %s", f.getName(), f);
+            }
+        }
+    }
+
+    public static List<Field> getAllFields(Class<?> cls) {
+        var currCls = cls;
+        var fields = new ArrayList<Field>();
+
+        while (currCls != Object.class) {
+            fields.addAll(Arrays.asList(currCls.getDeclaredFields()));
+            currCls = currCls.getSuperclass();
+        }
+
+        return fields;
     }
 }
