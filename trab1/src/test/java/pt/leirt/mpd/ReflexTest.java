a517d2a993395b3b9be6ad1980b9f8d2da377459
tommyfigs <tomasfvfx@outlook.com>
2024-04-04 14:30:39 +0100

    test: adição do test para o método saveToWriter

--- trab1/src/test/java/pt/leirt/mpd/ReflexTest.java
+++ trab1/src/test/java/pt/leirt/mpd/ReflexTest.java
@@ -5,13 +5,21 @@ import pt.leirt.mpd.products.Resolution;
 import pt.leirt.mpd.products.Speaker;
 import pt.leirt.mpd.products.TV;
 
-import java.io.IOException;
+import java.io.*;
 
 import static pt.leirt.mpd.ReflexUtils.saveToFile;
+import static pt.leirt.mpd.ReflexUtils.saveToWriter;
 
 public class ReflexTest {
     @Test
     public void saveToFileTest() throws IOException, IllegalAccessException {
         saveToFile(StoreDB.iPhone15, "output.json");
+    }
+
+    @Test
+    public void saveToWriterTest() throws IOException, IllegalAccessException {
+        try(Writer bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            saveToWriter(StoreDB.iPhone15, bw);
+        }
     }
 }
