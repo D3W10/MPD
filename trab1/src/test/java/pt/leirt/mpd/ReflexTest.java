1979b5dee1e0bd16c2b42d6bd25bcb91c5e79eed
Daniel Nunes <danyribeironunes15@gmail.com>
2024-04-04 14:19:01 +0100

    fix: Improve ReflexTest

--- trab1/src/test/java/pt/leirt/mpd/ReflexTest.java
+++ trab1/src/test/java/pt/leirt/mpd/ReflexTest.java
@@ -10,17 +10,8 @@ import java.io.IOException;
 import static pt.leirt.mpd.ReflexUtils.saveToFile;
 
 public class ReflexTest {
-    private final static Store store = new Store();
-
-    static {
-        store.addCatalog(new TV("X95", "Sony", 3000, new Resolution(3840, 2160), 65.0))
-                .addCatalog(new Speaker("x300", "JBL", 100, 40))
-                .addCatalog(new Speaker("s250", "Samsung", 200, 60))
-                .addCatalog(StoreDB.jblCharge3);
-    }
-
     @Test
     public void saveToFileTest() throws IOException, IllegalAccessException {
-        saveToFile(new TV("X95", "Sony", 3000, new Resolution(3840, 2160), 65.0), "output.json");
+        saveToFile(StoreDB.iPhone15, "output.json");
     }
 }
