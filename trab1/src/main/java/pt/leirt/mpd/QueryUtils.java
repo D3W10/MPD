e33e2b5e15efaa6497c8f67f783ee67b4a19c16f
Daniel Nunes <danyribeironunes15@gmail.com>
2024-04-04 15:35:30 +0100

    feat: Add filter(), map() and flatMap() to QueryUtils

--- trab1/src/main/java/pt/leirt/mpd/QueryUtils.java
+++ trab1/src/main/java/pt/leirt/mpd/QueryUtils.java
@@ -1,6 +1,9 @@
 package pt.leirt.mpd;
 
+import java.util.ArrayList;
 import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class QueryUtils {
     public static <T, U> U reduce(Iterable<T> src, U initial, BiFunction<U, T, U> combiner) {
@@ -10,5 +13,36 @@ public class QueryUtils {
             result = combiner.apply(result, element);
 
         return result;
+    }
+
+    public static <T> Iterable<T> filter(Iterable<T> src, Predicate<T> pred) {
+        var result = new ArrayList<T>();
+
+        for (var f : src) {
+            if (pred.test(f))
+                result.add(f);
+        }
+
+        return result;
+    }
+
+    public static <T,U> Iterable<U> map(Iterable<T> src, Function<T,U> mapper) {
+        var res = new ArrayList<U>();
+
+        for (var e: src)
+            res.add(mapper.apply(e));
+
+        return res;
+    }
+
+    public static <T,U> Iterable<U> flatMap(Iterable<T> src, Function<T,Iterable<U>> mapper) {
+        var res = new ArrayList<U>();
+
+        for (var e: src) {
+            for (var u : mapper.apply(e))
+                res.add(u);
+        }
+
+        return res;
     }
 }
