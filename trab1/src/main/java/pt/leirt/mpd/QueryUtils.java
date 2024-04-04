1842bde80b4f67ca33f6bb6f134f87e646b91e6b
Daniel Nunes <danyribeironunes15@gmail.com>
2024-04-04 13:24:50 +0100

    chore: Move QueryUtils

--- trab1/src/main/java/pt/leirt/mpd/QueryUtils.java
+++ trab1/src/main/java/pt/leirt/mpd/QueryUtils.java
@@ -1,13 +1,14 @@
-package pt.leirt.mpd.products;
+package pt.leirt.mpd;
 
 import java.util.function.BiFunction;
 
 public class QueryUtils {
     public static <T, U> U reduce(Iterable<T> src, U initial, BiFunction<U, T, U> combiner) {
         U result = initial;
-        for (T element : src) {
+
+        for (T element : src)
             result = combiner.apply(result, element);
-        }
+
         return result;
     }
 }
